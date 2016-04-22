ad1863a3c15954a5498b4e8cc9cd9da6bd45df3d
nbermudez <nbermudez@blizzard.com>
2016-04-22 15:33:04 -0700

    Adding remaining files

---
+++ NumbersEx/Completed/2EtoNthDigit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+                var e = Math.E;
+
+                Console.Write("How many digits of e would you like? ");
+
+                var eDigits = Convert.ToInt32(Console.ReadLine());
+
+                double roundede = Math.Round(e, digitCheck(eDigits));
+
+                Console.WriteLine("e Rounded to {0} digits is: {1}",eDigits,roundede);
+
+            } while (true);
+
+        }
+        /// <summary>
+        /// Checks number of digits return max 15 is >=14, else returns desired digits
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        public static int digitCheck(int digits)
+        {
+            if (digits >= 14)
+            {
+                Console.WriteLine("Max digits are 15. Will return 15 decimal spaces.");
+                return 15;
+            }
+            else
+            {
+                return digits;
+            }
+        }
+    }
+}
---
+++ NumbersEx/Completed/3Fibonacci.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("How many digits of the fibonnaci sequence would you like to display? (Max is 30) ");
+
+                var fibDigits = digitCheck(Convert.ToInt32(Console.ReadLine()));
+
+                //creates array of fib sequence up to specified digit count
+                int[] fibArray = new int[fibDigits];
+
+                for (int i = 0; i <= fibDigits-1; i++)
+                {
+                    if (i <= 1)
+                    {
+                        fibArray[i] = i;
+                    }
+                    else
+                    {
+                        fibArray[i] = fibArray[i-1] + fibArray[i - 2];
+                    }
+                }
+
+                //prints out array
+                foreach (int value in fibArray)
+                {
+                    Console.Write("{0} ", value);
+                }
+
+                Console.WriteLine("\n");
+
+            } while (true);
+
+        }
+       
+        //checks for max number of digits
+        public static int digitCheck(int digits)
+        {
+            if (digits >= 31)
+            {
+                Console.WriteLine("Max digits are 30. Will return 30 digits total.");
+                return 30;
+            }
+            else
+            {
+                return digits;
+            }
+        }
+    }
+}
