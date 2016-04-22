61b20e3b13e9b018ebd198beb9d6fe2b2bf70d02
nbermudez <nbermudez@blizzard.com>
2016-04-22 16:25:04 -0700

    Adding prime number list to project, WIP

--- NumbersEx/4PrimeFact.cs
+++ NumbersEx/4PrimeFact.cs
@@ -24,22 +24,33 @@ namespace NumbersEx
                 }
 
                 //stores array of first 11 prime numbers
-                int[] primeArray = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
+                //int[] primeArray = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
+                List<int> primeList = new List<int>();
+
+                for (int i = 2; i <= number; i++)
+                {
+                    if(Program.isPrime(number))
+                    {
+                        primeList.Add(i);
+                  
+                    }
+                }
+
                 List<int> factorsList = new List<int>();
               
                 //Run through primeArray list to check for primes
-                for(int i=0; i<primeArray.Length;i++)
+                for(int i=0; i<primeList.Count;i++)
                 {
                    //if prime is larger than number stop loop
-                    if (primeArray[i] > number)
+                    if (primeList[i] > number)
                     {
                         break;
                     }
                     //Save number if it divides correctly, restarts loop with modified number
-                    if (number % primeArray[i] == 0)
+                    if (number % primeList[i] == 0)
                     {
-                        factorsList.Add(primeArray[i]);
-                        number = number / primeArray[i];
+                        factorsList.Add(primeList[i]);
+                        number = number / primeList[i];
                         i = -1;
                     }
                 }
@@ -53,6 +64,27 @@ namespace NumbersEx
             } while (true);
 
         }
-       
+
+        static bool isPrime(int number)
+        {
+            if (number == 2 || number == 3)
+                return true;
+            if(number % 2 == 0 || number%3 == 0)
+                return false;
+            
+            var i = 5;
+            var w = 2;
+
+            while(i * i <= number){
+                if(number % i == 0)
+                    return false;
+
+                i += w;
+                w = 6 - w;
+            }
+
+
+            return true;
+        }
     }
 }
