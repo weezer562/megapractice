534c3268b4fa17cfe5944e337361c9f577c37200
nbermudez <nbermudez@blizzard.com>
2016-04-28 14:50:09 -0700

    Finished ChangeMaker

---
+++ NumbersEx/8ChangeReturn.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersEx
+{
+    class ChangeMaker
+    {
+        static void Main(string[] args)
+        {
+            double cost = 0;
+            double payment = 0;
+         
+            do
+            {
+                Console.Write("How much did the item cost? ");
+                cost = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("How much money did you pay? ");
+                payment = Convert.ToDouble(Console.ReadLine());
+
+                double changeDue = payment - cost;
+
+                Console.WriteLine("\nChange is ${0} \n", changeDue);
+
+                printChange(changeDue);
+
+
+            } while (true);
+
+        }
+
+        static void printChange(double changeDue)
+        {
+            int zero = 0;
+            int dollarAmount = (int)changeDue;
+      
+            int cents = Convert.ToInt32((changeDue - Convert.ToDouble(dollarAmount)) * 100);
+            int totalInCents = dollarAmount * 100 + cents;
+
+            if (totalInCents > 25)
+            {
+                int quarters = totalInCents / 25;
+                Console.WriteLine("Quarters: {0}", quarters);
+                totalInCents = totalInCents - quarters * 25;
+            }
+            else
+            {
+                Console.WriteLine("Quarters: {0}",zero);
+            }
+
+            if (totalInCents > 10)
+            {
+                int dimes = totalInCents / 10;
+                Console.WriteLine("Dimes:     {0}", dimes);
+                totalInCents = totalInCents - dimes * 10;
+            }
+            else
+            {
+                Console.WriteLine("Dimes:     {0}", zero);
+            }
+
+            if (totalInCents > 5)
+            {
+                int nickels = totalInCents / 5;
+                Console.WriteLine("Nickels:   {0}", nickels);
+                totalInCents = totalInCents - nickels * 5;
+            }
+            else
+            {
+                Console.WriteLine("Nickels:   {0}", zero);
+            }
+
+            if (totalInCents > 1)
+            {
+                int pennies = totalInCents / 1;
+                Console.WriteLine("Pennies:   {0}\n", pennies);
+                totalInCents = totalInCents - pennies * 1;
+            }
+            else
+            {
+                Console.WriteLine("Pennies:   {0}\n", zero);
+            }
+
+          
+        }
+
+    }
+}
---
+++ NumbersEx/Completed/7MortgageCalc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersEx
+{
+    class MortgageCalculator
+    {
+        static void Main(string[] args)
+        {
+            int term;
+            int interval;
+            double interest;
+            double loan;
+
+
+            do
+            {
+                Console.Write("Enter your Loan Amount: ");
+                loan = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter your interest: ");
+                interest = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter your term amount in years: ");
+                term = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Choose your interval 12-Monthly, 52-Weekly, 365-Daily: (Enter 12, 52, or 365) ");
+                interval = Convert.ToInt32(Console.ReadLine());
+
+                string intervalString = " ";
+
+                switch (interval)
+                {
+                    case 12:
+                        intervalString = "Monthly";
+                        break;
+                    case 52:
+                        intervalString = "Weekly";
+                        break;
+                    case 365:
+                        intervalString = "Daily";
+                        break;
+                    default:
+                        break;
+                }
+
+                double rate = (interest / 100) / interval;
+                int payments = term * interval;
+
+                double mortgage = (rate / (1-(1 / Math.Pow((1 + rate), payments)))) * loan;
+
+                Console.WriteLine("\nYour {0} mortgage payment is ${1}\n", intervalString, Math.Round(mortgage,2));
+
+            } while (true);
+
+        }
+       
+    }
+}
